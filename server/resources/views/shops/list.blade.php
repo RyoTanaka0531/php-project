6f8db22b1b2a42d03cea84b0a6fc8b51f990b607
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-18 22:31:37 +0900

    add mapMarker

--- server/resources/views/shops/list.blade.php
+++ server/resources/views/shops/list.blade.php
@@ -11,16 +11,21 @@
         </style>
         <script>
             function initMap(){
-
-                var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
+                var MyLatLng = new google.maps.LatLng(35.465700, 139.622138); //経度と緯度を指定
                 var Options = {
                     zoom:15,
                     center: MyLatLng,
                     mapTypeId: 'roadmap'
                 };
                 var map = new google.maps.Map(document.getElementById('map'), Options);
+                var markerOptions = {
+                    map:map, //マーカーを生成したいマップを指定
+                    position: MyLatLng, //どこにマーカーを生成するのか
+                };
+                //マーカーを生成するMarkerクラス
+                var marker = new google.maps.Marker(markerOptions);
             }
-        </script>
+            </script>
             <script defer src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAP_API')}}&callback=initMap"></script>
         <ul id="shop-list"></ul>
     </div>
